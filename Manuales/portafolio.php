d1c829546a69452ede75cbe606669eafd1f43d16
JaiderR2003 <jarodriguez98800@misena.edu.co>
2021-09-27 15:13:53 -0500

    Segundo avance del portafolio

---
+++ Manuales/portafolio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Jaider Andres Rodriguez</title>
+</head>
+<body>
+    <div class="contenedor">
+        <header>
+            <div class="contenedor__logo">
+                <h1>Jaider Andres Rodriguez</h1>
+                <p>DESARROLLADOR WEB</p>
+            </div>
+            <form>
+                <input type="text" class="buscador" id="buscador" placeholder="Buscar">
+            </form>
+        </header>
+    </div>
+
+</body>
+</html>
