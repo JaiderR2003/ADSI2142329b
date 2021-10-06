c0533faec4ad215dbaa2aa48e9b102eb201dfe4b
JaiderR2003 <jarodriguez98800@misena.edu.co>
2021-10-06 18:34:34 -0500

    Creacion del CRUD y cambio de nombre la carpeta principal

---
+++ Portafolio/vista/portafolio.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../Css/portafolio.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <div class="barra">
+            <h2>Menu<img src="../Imagen/MENU2.png"></h2>
+
+            <ul>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="conocimientos.html">Conocimientos</a></li>
+                <li><a href="#">Proyectos</a></li>
+            </ul>
+            
+        </div>
+    </header>
+
+    <section class="box">
+        <img src="../Imagen/yo.jpg" width="180" alt="Jaider Rodriguez" class="box__imagen">
+        <h1 class="box__titulo">Jaider Rodriguez</h1>
+        <h2 class="box__h2">Desarrollador web</h2>
+        <p class="box__parrafo">Yo soy jaider Andres Rodriguez Rodriguez. Soy un Aprendiz del sena que estudia desarrollo web. <br>
+            Desde que estoy estudiando desarrollo web, me ha parecido fascinante lo que se puede realizar en los lenguajes de programación. Soy una persona que le gusta trabajar y esforzarse.
+
+        </p>
+        <ul box__ul>
+            <li class="box--li"><a class="box--li--a" href="https://www.facebook.com/jaiderandre.rodriguez/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+            <li class="box--li"><a class="box--li--a" href="https://github.com/JaiderR2003/ADSI2142329b"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+            <li class="box--li"><a class="box--li--a" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            <li class="box--li"><a class="box--li--a" href="https://www.instagram.com/rjaiderandres/?hl=es"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        </ul>
+    </section>
+</body>
+</html>
